c5febf9e4e26f5ad9412d263a6ea7b2926c2d921
kflash06 <ketan.shinde@sparkeighteen.com>
2022-03-25 21:03:35 +0530

    25th March 2022

--- 24th march 2022/seed-data.sql
+++ 24th march 2022/seed-data.sql
@@ -11,32 +11,33 @@ VALUES
 INSERT INTO nobroker.listingcriteria_type
 (listingcriteriatype_id, listing_criteria_type, value)
 VALUES
-(0, 'gender', 'male'),
-(1, 'gender', 'female'),
-(2, 'room', 'single'),
-(3, 'room', 'double'),
-(4, 'room', 'triple'),
-(5, 'room', 'four'),
-(6, 'room', 'four+'),
-(7, 'tenanttype', 'rent'),
-(8, 'tenanttype', 'pg'),
-(9, 'tenanttype', 'flatmates'),
-(10, 'tenanttype', 'hostel');
+(1, 'gender', 'male'),
+(2, 'gender', 'female'),
+(3, 'room', 'single'),
+(4, 'room', 'double'),
+(5, 'room', 'triple'),
+(6, 'room', 'four'),
+(7, 'room', 'four+'),
+(8, 'tenanttype', 'rent'),
+(9, 'tenanttype', 'pg'),
+(10, 'tenanttype', 'flatmates'),
+(11, 'tenanttype', 'hostel');
 
 INSERT INTO nobroker.nearby_type
 (nearby_type_id, "type")
-(0, 'hospital'),
-(1, 'railway'),
-(2, 'restaurant'),
-(3, 'bank');
+VALUES 
+(1, 'hospital'),
+(2, 'railway'),
+(3, 'restaurant'),
+(4, 'bank');
 
 INSERT INTO nobroker.area
 (area_id, area_name, latitude, longitude, radius)
 VALUES
-(0, 'Kopar Khairane', '19.102769', '73.009001', 5.5),
-(0, 'Vashi', '19.0745', '72.9978', 5.5),
-(0, 'Rabale', '19.1463', '73.0081', 5.5),
-(0, 'Airoli', '19.1590', '72.9986', 5.5);
+(1, 'Kopar Khairane', '19.102769', '73.009001', 5.5),
+(2, 'Vashi', '19.0745', '72.9978', 5.6),
+(3, 'Rabale', '19.1463', '73.0081', 5.7),
+(4, 'Airoli', '19.1590', '72.9986', 5.8);
 
 
 
---
+++ 25th march 2022/README.md
@@ -0,0 +1,51 @@
+# 25-MARCH-2022
+
+## First Half
+
+- Revised / collected thoughts on what all I have accomplished so far in this assignment of NoBroker Clone and what more I need to come up with next.
+
+- Started Adding Additional Features on API spec sheet
+
+### Videos
+
+- none
+
+### Assignment 
+
+- NoBroker Clone - In-Progress
+
+### Doubts
+
+- No
+
+### Links
+
+- null
+
+## Second Half
+ 
+- Continued Adding feature in the sheet 
+
+- Thinking of what more is needed and corrected some of the features added before 
+
+- Read a blog on Spring Security - Roles and Privileges
+
+### Videos
+
+- Null
+
+### Assignment 
+
+- NoBroker Clone - In-Progress
+
+### Doubts
+
+- Unable to understand what route is called from API to Perform CRUD operation in Filter feature
+
+### Links
+
+- https://www.baeldung.com/role-and-privilege-for-spring-security-registration
+
+- Here is the Link to my drive file containing documents related to NoBroker Clone
+
+- https://drive.google.com/drive/u/0/folders/1dApAhNUtTCLiQQdvUst7MchyB7XoZ167
